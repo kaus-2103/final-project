0a03ad338afb7713ba47c5daf009eea75ccfe0c9
Kaushik Roy <63772738+kaus-2103@users.noreply.github.com>
2024-05-09 12:46:06 +0600

    implemented block function

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,12 @@
+class Users::SessionsController < Devise::SessionsController
+  def create
+    user = User.find_by(email: params[:user][:email])
+
+    if user && user.valid_password?(params[:user][:password]) && user.status
+      super
+    else
+      flash[:alert] = "Your account is blocked or does not exist."
+      redirect_to new_user_session_path
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get 'user/manager'
   get 'user/manager'
-  devise_for :users
+  devise_for :users, controllers: {
+        sessions: 'users/sessions'
+      }
   root 'home#index'
   resources :users do
     member do
