9339d4a067941a1977509f038e6e86d6ff11a74e
Kaushik Roy <63772738+kaus-2103@users.noreply.github.com>
2024-05-26 12:22:20 +0600

    lil fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,36 +7,36 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-# User.create!(
-#     name: 'Admin',
-#   email: 'admin@example.com',
-#   password: 'password',
-#   password_confirmation: 'password',
-#   admin: true,
-#   status: true
-# )
-
-# # Create Regular Users
-# 5.times do |i|
-#   User.create!(
-#     name: "User #{i + 1}",
-#     email: "user#{i + 1}@example.com",
-#     password: 'password',
-#     password_confirmation: 'password',
-#     admin: false,
-#     status: true
-#   )
-# end
-
-
-Item.create!(
-  name: 'Example Item',
-  description: 'This is an example item',
-  category: 'Example Category',
-  image: 'example.jpg',
-  user_id: 1,
-  collection_id: 1,
-  tags: ['example', 'seed']
+User.create!(
+    name: 'Admin',
+  email: 'admin@example.com',
+  password: 'password',
+  password_confirmation: 'password',
+  admin: true,
+  status: true
 )
+
+# Create Regular Users
+5.times do |i|
+  User.create!(
+    name: "User #{i + 1}",
+    email: "user#{i + 1}@example.com",
+    password: 'password',
+    password_confirmation: 'password',
+    admin: false,
+    status: true
+  )
+end
+
+
+# Item.create!(
+#   name: 'Example Item',
+#   description: 'This is an example item',
+#   category: 'Example Category',
+#   image: 'example.jpg',
+#   user_id: 1,
+#   collection_id: 1,
+#   tags: ['example', 'seed']
+# )
 
 
