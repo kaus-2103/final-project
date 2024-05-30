218efdf4d923490db587095169372ea4f410a9bc
Kaushik Roy <63772738+kaus-2103@users.noreply.github.com>
2024-05-30 12:20:21 +0600

    polishing progressed somewhat

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+    def like_button_for(item, user)
+        if item.liked_by?(user)
+          button_to t('unlike'), item_like_path(item), method: :delete, class: "btn btn-danger"
+        else
+          button_to t('like'), item_like_path(item), class: "btn btn-primary"
+        end
+      end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,12 @@ class Item < ApplicationRecord
   belongs_to :collection
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :liked_users, through: :likes, source: :user
   has_one_attached :pic
+
+  def liked_by?(user)
+    liked_users.include?(user)
+  end
   def copy_and_transform_custom_fields_from_collection
     self.custom_field = transform_custom_fields(collection.custom_fields)
   end
