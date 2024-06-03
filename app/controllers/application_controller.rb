29cd388b7d699e18b6da04bd5550a07cb4e10bfc
Kaushik Roy <63772738+kaus-2103@users.noreply.github.com>
2024-06-03 15:52:03 +0600

    updating login problem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
     before_action :set_locale
+    protect_from_forgery with: :exception
+    before_action :configure_permitted_parameters, if: :devise_controller?
 
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
@@ -7,5 +9,9 @@ class ApplicationController < ActionController::Base
 
   def default_url_options
     { locale: I18n.locale }
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :status, :admin)}
   end
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,7 +2,7 @@ class Users::SessionsController < Devise::SessionsController
   def create
     user = User.find_by(email: params[:user][:email])
 
-    if user && user.valid_password?(params[:user][:password]) && user.status
+    if user && user.valid_password?(params[:user][:password]) && (user.status== nil || user.status== true)
       super
     else
       flash[:alert] = "Your account is blocked or does not exist."
